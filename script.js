5d89d163c27b3f84bf18ce343da132c3228243d4
Richard Hayes <rhshadowgreen@gmail.com>
2019-12-27 17:31:15 -0500

    wrap code in async function

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 // Create a peer
 let c = new PeerCloud();
 
-// Join the peer cloud (if the page URL is an invite link)
-c.join();
+(async () => {
+    // Join the peer cloud (if the page URL is an invite link)
+    c.join();
 
-// Print an invite URL
-let url = await c.getInviteURL();
-console.log("Invite:", url);
+    // Print an invite URL
+    let url = await c.getInviteURL();
+    console.log("Invite:", url);
 
-// Print all incomming messages
-c.listen( msg => {
-    console.log(msg);
-});
+    // Print all incomming messages
+    c.listen( msg => {
+        console.log(msg);
+    });
 
-// Send a message to all users in the peer cloud
-c.broadcast("Hello world!");
+    // Send a message to all users in the peer cloud
+    c.broadcast("Hello world!");
+})();
